8cd69706b07e5a16e1826c905ebffa93fb11fcde
bethcreate <79142026+bethcreate@users.noreply.github.com>
2021-06-17 11:05:43 +0300

    Add files via upload

---
+++ python_test.py
@@ -0,0 +1,105 @@
+x = [100,110,120,130,140,150]
+my_list=[element*5 for element in x]
+print(my_list)
+
+    
+def divisible_by_three(n):
+    range(1 ,n)
+    if n%3==0:
+        print("all numbers divisible by three")
+
+
+
+x = [[1,2],[3,4],[5,6]]
+flat_list = []
+for a in range(len(x)):
+    for b in range(len(x[a])):
+        flat_list.append(x[a] [b])
+
+print(flat_list)
+
+
+def smallest(nums):
+    small = 1
+    for a in nums:
+        small += a
+        return small
+print(smallest(2,5,1,7))
+
+
+def remove_duplicates():
+    x = ['a','b','a','e','d','b','c','e','f','g','h']
+    b=set(x)
+    for x in b:
+        x.append(x.remove())
+    return x
+
+
+def divisible_by_seven():
+    for a in range(100, 200):
+        if(a%7==0):
+            print("a")
+
+
+
+def greet_students():
+    students = [{"age": 19, "name": "Eunice"}, {"age": 21, "name": "Agnes"},
+     {"age": 18, "name": "Teresa"}, {"age": 22, "name": "Asha"}]
+
+
+
+
+
+class Rectangle():
+    def __init(self, width, length):
+        self.width=width
+        self.length=length
+
+    def area(self, A):
+        return f"the area is {self.width}*{self.length}"
+
+    def perimeter(self, P):
+        return f"2 {self.width}* {self.length} "
+     
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+ 
+ 
+
+
